c72067757ae32428d63a60aafde777d69f31f211
Dennis Guiboche <dguiboche2@academic.rrc.ca>
2026-05-07 14:56:03 -0500

    more practice done for video 2

---
+++ chapter_1/script.js
@@ -0,0 +1,28 @@
+// alert('ALL YOUR DATA IS MINE THANKS FOR OPENING THE SITE HAHAHAH');
+
+
+let logCount= 0;
+function myLog(message) {
+    console.log(message);
+    logCount++;
+}
+
+
+console.log(1);
+
+
+let age = 30;
+let year = 2026;
+
+console.log(age, year);
+
+age = 15
+console.log(age)
+
+
+const points = 100;
+
+console.log(points);
+
+var score = 75; //old way of declaring variables, avoid using it
+console.log(score);
